9c2cf8167fdea05fb9c5d0f7c9e7222f163b9901
Jonghyun Oh <sjaiz1717@gmail.com>
2026-01-20 20:07:34 +0900

    [Gold III] Title: 치즈, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,66 +1,32 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
-# 입력, O(N * M)
 N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-total = 0
-board = []
+q = [(0, 0)]
+d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+ans = -1
 
-for i in range(N):
-    board.append(list(map(int, input().split())))
-    total = total + sum(board[i])
+while q:
+    _q = [] # 다음에 둘 곳
+    while q:
+        x, y = q.pop()
+        for dx, dy in d:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < M:
+                v = arr[nx][ny]
+                if v < 0: # 이미 방문
+                    continue
+                elif not v: # 빈 자리
+                    arr[nx][ny] = -1
+                    q.append((nx, ny))
+                elif v == 1: # 치즈
+                    arr[nx][ny] += 1
+                else: # v > 1, 한 번 방문했는데 재방문
+                    arr[nx][ny] = -1
+                    _q.append((nx, ny)) # 다음 turn(한 시간 후) 방문
+    q = _q
+    ans += 1
 
-# test1: print(total) print(board[N-1][M-1])
-
-# 외부 공기 구분, O(N * M)
-def spread():
-    visited = [[False] * M for _ in range(N)]
-    queue = deque([(0,0)])
-    visited[0][0] = True
-
-    while queue:
-        r, c = queue.popleft()
-        board[r][c] = -1
-        for nr, nc in (r-1,c), (r+1,c), (r,c-1), (r,c+1):
-            if (0 <= nr < N) and (0 <= nc < M):
-                if board[nr][nc] != 1 and not visited[nr][nc]:
-                    visited[nr][nc] = True
-                    queue.append((nr, nc))
-
-# test2: spread() print(board)
-
-# 녹을 치즈 구분 (상수)
-def is_valid(r, c):
-    count = 0
-    for nr, nc in (r-1,c), (r+1,c), (r,c-1), (r,c+1):
-        if board[nr][nc] == -1:
-            count = count + 1
-    
-    if count >= 2:
-        return True
-
-    return False
-
-# 결과 계산, O()
-time = 0
-
-while True:
-    spread()
-
-    melt = []
-    for i in range(N):
-        for j in range(M):
-            if board[i][j] == 1:
-                if is_valid(i,j):
-                    melt.append((i,j))
-
-    for r, c in melt:
-        board[r][c] = -1
-        total = total - 1
-
-    time = time + 1
-    if total == 0:
-        print(time)
-        break
+print(ans)
