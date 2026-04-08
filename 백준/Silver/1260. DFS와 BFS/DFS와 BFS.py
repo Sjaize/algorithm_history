8d1245b3e5b98fa282cb8f70a46b59780f3a0d15
Jonghyun Oh <sjaiz1717@gmail.com>
2026-04-08 21:41:55 +0900

    [Silver II] Title: DFS와 BFS, Time: 316 ms, Memory: 35268 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,8 +1,7 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
 
 N, M, V = map(int, input().split())
+
 graph = [[] for _ in range(N+1)]
 
 for _ in range(M):
@@ -10,34 +9,31 @@ for _ in range(M):
     graph[a].append(b)
     graph[b].append(a)
 
-# 노드 방문 순서 정렬
 for i in range(N+1):
     graph[i].sort()
 
 def dfs(v, visited):
     visited[v] = True
     print(v, end = " ")
-    for node in graph[v]:
-        if not visited[node]:
-            dfs(node, visited)
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i, visited)
 
 def bfs(v, visited):
     queue = deque([v])
     visited[v] = True
-
     while queue:
-        cur = queue.popleft()
-        print(cur, end = " ")
+        x = queue.popleft()
+        print(x, end = " ")
+        for i in graph[x]:
 
-        for nxt in graph[cur]:
-            if not visited[nxt]:
-                visited[nxt] = True
-                queue.append(nxt)
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
 
-# 결과 출력
 visited = [False] * (N+1)
 dfs(V, visited)
 print()
-
 visited = [False] * (N+1)
 bfs(V, visited)
